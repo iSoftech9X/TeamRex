11e637000c407b1dd714ee548915f60e60e28442
sandeep <murarishettysandeep688@gmail.com>
2025-09-10 17:18:09 +0530

    cors  update

--- src/main/java/com/chatapp/config/SecurityConfig.java
+++ src/main/java/com/chatapp/config/SecurityConfig.java
@@ -116,6 +116,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("https://4iwjzbgtmd.eu-central-1.awsapprunner.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
